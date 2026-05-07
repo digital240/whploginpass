1606cd186562f371c050c7c184456f29a83e6418
digital240 <digital@whpjewellers.in>
2026-05-07 11:39:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const tokenStore = {};
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-shop-domain');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-shop-domain, x-user-token');
   if (req.method === 'OPTIONS') return res.status(200).end();
   next();
 });
